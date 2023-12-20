8fef2ebf2bf9da5101bb124849e3af00e157892f
cocaine_coder <tracywang1997@outlook.com>
2023-12-20 10:01:13 +0800

    增加标注导出dxf颜色渲染

--- example/map.ts
+++ example/map.ts
@@ -447,8 +447,14 @@ map.on('load', () => {
     map.addControl(measure2Control);
     map.addControl(switchLayerControl);
 
-    map.addControl(new MarkerControl({
+    const markerControl = new MarkerControl({
         markerOptions: {
+            layers: [{
+                id: "35ebd84f-1c75-42b3-ba76-568cac847cf5",
+                name: "test",
+                date: Date.now()
+            }],
+            featureCollection: { "type": "FeatureCollection", "features": [{ "type": "Feature", "geometry": { "type": "Polygon", "coordinates": [[[120.33795166015392, 31.155843389096844], [120.33245849609165, 30.95466933754993], [120.68470764159827, 30.975865573909786], [120.64350891112883, 31.155843389096844], [120.33795166015392, 31.155843389096844]]] }, "properties": { "id": "2dae1cb3-f2f9-4c34-b500-8288ed258639", "name": "标注", "layerId": "35ebd84f-1c75-42b3-ba76-568cac847cf5", "date": 1703034323051, "style": { "textSize": 14, "textColor": "black", "textHaloColor": "white", "textHaloWidth": 1, "pointIcon": "标1.png", "pointIconColor": "#ff0000", "pointIconSize": 0.3, "lineColor": "#0000ff", "lineWidth": 3, "polygonColor": "#0000ff", "polygonOpacity": 0.5, "polygonOutlineColor": "#000000", "polygonOutlineWidth": 2 } }, "id": "2dae1cb3-f2f9-4c34-b500-8288ed258639" }, { "type": "Feature", "geometry": { "type": "LineString", "coordinates": [[120.2605379098901, 30.869971361182337], [120.38410169141792, 30.8711497871234], [120.48501211299799, 30.88764622882202], [120.57974434550329, 30.878220038493666], [120.66212019985676, 30.84581043692205], [120.72184269426003, 30.848167868226483]] }, "properties": { "id": "04abb291-814c-4ad1-9f98-6277cdcefd27", "name": "标注", "layerId": "35ebd84f-1c75-42b3-ba76-568cac847cf5", "date": 1703036360925, "style": { "textSize": 19, "textColor": "#00ffff", "textHaloColor": "#000000", "textHaloWidth": 1, "pointIcon": "标1.png", "pointIconColor": "#ff0000", "pointIconSize": 0.3, "lineColor": "#ff0000", "lineWidth": 6, "polygonColor": "#0000ff", "polygonOpacity": 0.5, "polygonOutlineColor": "#000000", "polygonOutlineWidth": 2 } }, "id": "04abb291-814c-4ad1-9f98-6277cdcefd27" }, { "type": "Feature", "geometry": { "type": "Point", "coordinates": [120.2530240890847, 31.09089416247764] }, "properties": { "id": "55942abd-dc73-4f45-9247-2a7926339bde", "name": "标注", "layerId": "35ebd84f-1c75-42b3-ba76-568cac847cf5", "date": 1703036400053, "style": { "textSize": 19, "textColor": "#00ffff", "textHaloColor": "#000000", "textHaloWidth": 1, "pointIcon": "标1.png", "pointIconColor": "#ff0000", "pointIconSize": 0.3, "lineColor": "#ff0000", "lineWidth": 6, "polygonColor": "#0000ff", "polygonOpacity": 0.5, "polygonOutlineColor": "#000000", "polygonOutlineWidth": 2 } }, "id": "55942abd-dc73-4f45-9247-2a7926339bde" }] },
             layerOptions: {
                 extraInfo: f => {
                     const g = f.geometry;
@@ -472,7 +478,9 @@ map.on('load', () => {
                 }
             }
         }
-    }));
+    });
+    map.addControl(markerControl);
+    markerControl.open = true;
 
     new ExtendControlsWrapper([measure2Control, switchLayerControl.extendControl]);
 })
--- lib/features/marker/exporter/ExportConverter.ts
+++ lib/features/marker/exporter/ExportConverter.ts
@@ -1,8 +1,11 @@
 import tokml from '@maphubs/tokml';
-import { DxfWriter, HatchBoundaryPaths, HatchPolylineBoundary, HatchPredefinedPatterns, LWPolylineVertex, pattern, vertex } from '@tarikjabiri/dxf';
-import { ExportGeoJsonType } from '../types';
-import { date } from 'wheater';
+import { Colors, DxfWriter, HatchBoundaryPaths, HatchPolylineBoundary, HatchPredefinedPatterns, LWPolylineVertex, TrueColor, pattern, vec3_t, vertex } from '@tarikjabiri/dxf';
+import { array, date } from 'wheater';
+import centroid from '@turf/centroid';
+
+import { ExportGeoJsonType, MarkerFeatrueProperties } from '../types';
 import { coordConverter } from '../../../common/proj';
+
 
 export type FileType = 'dxf' | 'kml' | 'geojson' | 'csv';
 
@@ -16,73 +19,93 @@ export class DxfConverter implements IExportConverter {
 
     convert(geojson: ExportGeoJsonType): string {
         const dxf = new DxfWriter();
-        const featrues = geojson.type === "Feature" ? [geojson] : geojson.features;
 
+        const featrues = geojson.type === "Feature" ? [geojson] : geojson.features;
         featrues.forEach(f => {
+            const props = f.properties;
             switch (f.geometry.type) {
                 case "Point":
-                    const point = coordConverter.wgs84g_to_cgcs2000p([f.geometry.coordinates[0], f.geometry.coordinates[1]]);
-                    dxf.addPoint(point[0], point[1], 0);
+                    this.circle(dxf, f.geometry.coordinates, props);
                     break;
                 case "MultiPoint":
                     f.geometry.coordinates.forEach(x => {
-                        const point = coordConverter.wgs84g_to_cgcs2000p([x[0], x[1]]);
-                        dxf.addPoint(point[0], point[1], 0);
+                        this.circle(dxf, x, props);
                     })
                     break;
 
                 case "LineString":
-                    const points = f.geometry.coordinates.map(coord => {
-                        const point = coordConverter.wgs84g_to_cgcs2000p([coord[0], coord[1]]);
-                        return { point: { x: point[0], y: point[1] } } as LWPolylineVertex;
-                    })
-                    dxf.addLWPolyline(points, { thickness: f.properties.style.lineWidth });
+                    this.polyline(dxf, f.geometry.coordinates, props);
                     break;
 
                 case "MultiLineString":
                     f.geometry.coordinates.forEach(x => {
-                        const points = x.map(coord => {
-                            const point = coordConverter.wgs84g_to_cgcs2000p([coord[0], coord[1]]);
-                            return { point: { x: point[0], y: point[1] } } as LWPolylineVertex;
-                        })
-                        dxf.addLWPolyline(points, { thickness: f.properties.style.lineWidth });
+                        this.polyline(dxf, x, props);
                     });
                     break;
 
                 case "Polygon":
-                    const solid = pattern({
-                        name: HatchPredefinedPatterns.SOLID,
-                    });
-
-                    const boundary = new HatchBoundaryPaths();
-                    boundary.addPolylineBoundary(new HatchPolylineBoundary(f.geometry.coordinates[0].map(coord => {
-                        const point = coordConverter.wgs84g_to_cgcs2000p([coord[0], coord[1]]);
-                        return vertex(point[0], point[1]);
-                    })));
-
-                    dxf.addHatch(boundary, solid);
-
+                    this.polygon(dxf, f.geometry.coordinates, props);
                     break;
 
                 case "MultiPolygon":
                     f.geometry.coordinates.forEach(x => {
-                        const solid = pattern({
-                            name: HatchPredefinedPatterns.SOLID,
-                        });
-
-                        const boundary = new HatchBoundaryPaths();
-                        boundary.addPolylineBoundary(new HatchPolylineBoundary(x[0].map(coord => {
-                            const point = coordConverter.wgs84g_to_cgcs2000p([coord[0], coord[1]]);
-                            return vertex(point[0], point[1]);
-                        })));
-
-                        dxf.addHatch(boundary, solid);
+                        this.polygon(dxf, x, props);
                     });
                     break;
             }
         });
 
         return dxf.stringify();
+    }
+
+    private text(dxf: DxfWriter, position: GeoJSON.Position | vec3_t, options: MarkerFeatrueProperties) {
+        if (position instanceof Array) {
+            const point = coordConverter.wgs84g_to_cgcs2000p([position[0], position[1]]);
+            position = { x: point[0], y: point[1], z: 0 };
+        }
+
+        dxf.addText(position, (options.style.textSize ?? 10), options.name, {
+            trueColor: TrueColor.fromHex(options.style.textColor!).toString()
+        });
+    }
+
+    private circle(dxf: DxfWriter, position: GeoJSON.Position, options: MarkerFeatrueProperties, withText: boolean = true) {
+        const point = coordConverter.wgs84g_to_cgcs2000p([position[0], position[1]]);
+        const vec = { x: point[0], y: point[1], z: 0 } as vec3_t;
+
+        dxf.addCircle(vec, 10, { trueColor: TrueColor.fromHex(options.style.pointIconColor!).toString() });
+        if (withText)
+            this.text(dxf, vec, options);
+    }
+
+    private polyline(dxf: DxfWriter, positions: GeoJSON.Position[], options: MarkerFeatrueProperties, withText: boolean = true) {
+        const points = positions.map(p => {
+            const point = coordConverter.wgs84g_to_cgcs2000p([p[0], p[1]]);
+            return { point: { x: point[0], y: point[1] } } as LWPolylineVertex;
+        });
+
+        dxf.addLWPolyline(points, {
+            thickness: options.style.lineWidth,
+            trueColor: TrueColor.fromHex(options.style.lineColor!).toString()
+        });
+        if (withText)
+            this.text(dxf, { ...array.first(points)!.point, z: 0 }, options);
+    }
+
+    private polygon(dxf: DxfWriter, positionsArray: GeoJSON.Position[][], options: MarkerFeatrueProperties, withText: boolean = true) {
+        const solid = pattern({
+            name: HatchPredefinedPatterns.SOLID,
+        });
+
+        const boundary = new HatchBoundaryPaths();
+        boundary.addPolylineBoundary(new HatchPolylineBoundary(positionsArray[0].map(coord => {
+            const point = coordConverter.wgs84g_to_cgcs2000p([coord[0], coord[1]]);
+            return vertex(point[0], point[1]);
+        })));
+
+        dxf.addHatch(boundary, solid, { trueColor: TrueColor.fromHex(options.style.polygonColor!).toString() });
+        if (withText)
+            this.text(dxf, centroid({ type: 'Polygon', coordinates: positionsArray }).geometry.coordinates, options);
     }
 }
 
@@ -109,14 +132,14 @@ export class CSVConverter implements IExportConverter {
 
         return header + '\n' + features.map(f =>
             this.value2Cell(f.properties.name) + ',' +
-            date.formatDate(new Date(f.properties.date), 'yyyy-MM-dd HH:mm:ss')+ ',' +
-            this.value2Cell(JSON.stringify(f.properties.style))+ ',' +
-            f.geometry.type+ ',' +
+            date.formatDate(new Date(f.properties.date), 'yyyy-MM-dd HH:mm:ss') + ',' +
+            this.value2Cell(JSON.stringify(f.properties.style)) + ',' +
+            f.geometry.type + ',' +
             this.value2Cell(JSON.stringify(f.geometry))).join('\n');
     }
 
     private value2Cell(value: string) {
-        value = value.replace(/"/g,`""`);
+        value = value.replace(/"/g, `""`);
         return value.indexOf(',') >= 0 ?
             `"${value}"` : value;
     }
